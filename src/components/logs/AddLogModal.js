a0b0eac40027d6fb41b5b4f16fd88e8b577597ea
Othman Assas <othman_assas@hotmail.fr>
2019-07-04 23:22:59 +0200

    Add logs

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
 	const [message, setMessage] = useState('');
 	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState('');
 
-const onSubmit = () =>{
-	if(message === '' || tech === ''){
-		M.toast({html: 'Please enter a message and tech'})
-	}else {
-		console.log(message, tech, attention);
+	const onSubmit = () => {
+		if (message === '' || tech === '') {
+			M.toast({ html: 'Please enter a message and tech' })
+		} else {
 
-		 // Clear Fields
-		 setMessage('');
-		 setTech('');
-		 setAttention(false);
+			const newLog = {
+				message,
+				attention,
+				tech,
+				date: new Date()
+			}
 
+			addLog(newLog);
+
+			M.toast({ html: `Log added by ${tech}` })
+
+			// Clear Fields
+			setMessage('');
+			setTech('');
+			setAttention(false);
+
+		}
 	}
-}
 
 	return (
 		<div id='add-log-modal' className="modal" style={modalStyle}>
@@ -81,4 +94,9 @@ const modalStyle = {
 	height: '75%'
 }
 
-export default AddLogModal;
+AddLogModal.propTypes = {
+	addLog: PropTypes.func.isRequired,
+}
+
+
+export default connect(null, { addLog })(AddLogModal);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,32 +1,38 @@
-import {GET_LOGS,SET_LOADING, LOGS_ERROR} from '../actions/types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions/types';
 
 
 const initialState = {
-    logs:null,
-    current:null,
-    loading:false,
+    logs: null,
+    current: null,
+    loading: false,
     error: null
 }
 
 export default (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case GET_LOGS:
-            return{
+            return {
                 ...state,
-                logs:action.payload,
+                logs: action.payload,
+                loading: false
+            }
+        case ADD_LOG: 
+            return {
+                ...state,
+                logs: [...state.logs, action.payload],
                 loading:false
             }
         case SET_LOADING:
             return {
                 ...state,
-                loading:true
+                loading: true
             }
-            case LOGS_ERROR:
-                console.error(action.payload)
-                return {
-                    ...state,
-                    error:action.payload
-                }
+        case LOGS_ERROR:
+            console.error(action.payload)
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state;
     }
