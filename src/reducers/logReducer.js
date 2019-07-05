91a88473342febe4149663011e9d15d402a0bd88
Othman Assas <othman_assas@hotmail.fr>
2019-07-05 22:54:09 +0200

    Search logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,12 +1,13 @@
 import {
-    GET_LOGS, 
-    SET_LOADING, 
-    LOGS_ERROR, 
-    ADD_LOG, 
-    DELETE_LOG, 
-    SET_CURRENT, 
-    CLEAR_CURRENT, 
-    UPDATE_LOG 
+    GET_LOGS,
+    SET_LOADING,
+    LOGS_ERROR,
+    ADD_LOG,
+    DELETE_LOG,
+    UPDATE_LOG,
+    SEARCH_LOGS,
+    SET_CURRENT,
+    CLEAR_CURRENT,
 } from '../actions/types';
 
 
@@ -24,45 +25,50 @@ export default (state = initialState, action) => {
                 ...state,
                 logs: action.payload,
                 loading: false
-            }
-        case ADD_LOG: 
+            };
+        case ADD_LOG:
             return {
                 ...state,
                 logs: [...state.logs, action.payload],
-                loading:false
-            }
+                loading: false
+            };
         case DELETE_LOG:
             return {
                 ...state,
-                logs:state.logs.filter(log=>log.id !== action.payload),
-                loading:false
-            }
+                logs: state.logs.filter(log => log.id !== action.payload),
+                loading: false
+            };
         case UPDATE_LOG:
-            return{
+            return {
                 ...state,
                 logs: state.logs.map(log => log.id === action.payload.id ? action.payload : log)
-            }
+            };
+        case SEARCH_LOGS:
+            return {
+                ...state,
+                logs: action.payload
+            };
         case SET_CURRENT:
             return {
                 ...state,
-                current:action.payload
-            }
+                current: action.payload
+            };
         case CLEAR_CURRENT:
             return {
                 ...state,
-                current:null
-            }
+                current: null
+            };
         case SET_LOADING:
             return {
                 ...state,
                 loading: true
-            }
+            };
         case LOGS_ERROR:
             console.error(action.payload)
             return {
                 ...state,
                 error: action.payload
-            }
+            };
         default:
             return state;
     }
